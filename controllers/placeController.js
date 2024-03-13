5707b673753ff2169f40a401e66f018d38862968
Jaineel <jaineeljjp@gmail.com>
2024-03-13 18:31:58 +0530

    Contry done

---
+++ controllers/placeController.js
@@ -0,0 +1,73 @@
+const Place = require("../models/Places");
+
+
+module.exports = {
+    addPlaces: async (req, res, next) => {
+        const { country_id, description, imageUrl, location, title, rating, review, latitude, longitude, contact_id } = req.body;
+
+        try {
+            const newPlace = new Place({
+                country_id,
+                description,
+                imageUrl,
+                location,
+                contact_id,
+                title,
+                rating,
+                review,
+                latitude,
+                longitude
+            })
+
+            await newPlace.save();
+
+            res.status(201).json({ status: true })
+        } catch (error) {
+            return next(error)
+        }
+    },
+
+    getPlaces: async (req, res, next) => {
+        try {
+
+            const places = await Place.find({}, '_id review rating imageUrl title country_id')
+
+            res.status(200).json({ places })
+
+        } catch (error) {
+            return next(error)
+        }
+    },
+
+    getPlace: async (req, res, next) => {
+        const placeId = req.params.id;
+
+        try {
+
+            const place = await Place.findById(placeId, {createdAt: 0, updatedAt: 0, __v: 0})
+
+            res.status(200).json({ place })
+
+        } catch (error) {
+            return next(error)
+        }
+    },
+
+    getPlacesByCountry: async (req, res, next) => {
+        const countryId = req.params.id;
+        try {
+
+            const places = await Place.find({country_id: countryId}, {createdAt: 0, updatedAt: 0, __v: 0})
+
+            if(places.length === 0){
+                return res.status(200).json([])
+            }
+
+            res.status(200).json({ places })
+
+        } catch (error) {
+            return next(error)
+        }
+    },
+
+}
